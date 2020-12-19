82942e50594647dcb0cff953ea69bcf990731e99
Nicolás Bobb <nicobobb@gmail.com>
2020-12-18 23:53:23 -0300

    Firestore, Chakra UI, Absolute Imports

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,29 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css'
-import { useAuth } from '../lib/auth';
+import { Button, Heading, Text, Code } from '@chakra-ui/core';
 
-export default function Home() {
+import { useAuth } from '@/lib/auth';
+
+const Home = () => {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Gestor de Tareas</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Organizador de Tareas
-        </h1>
-
-        <div className={styles.grid}>
-            <p className="description">
-             Current user: <code>{auth?.user ? auth.user.email : 'None'}</code>
-            </p>
-            {auth?.user ? (
-              <button onClick={(e) => auth.signout()}>Sign Out</button>
-            ) : (
-              <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
-            )}
-            </div>
+      <main>
+        <Heading>Gestor de Tareas</Heading>
+        <Text>
+          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+        </Text>
+        {auth.user ? (
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
